a0a8c99b57d2a09a4b10fee060e0b747c4330ef3
dimasan92 <dimasan92@gmail.com>
2018-05-31 17:53:14 +0300

    fix

--- app/src/main/java/ru/geekbrains/weatherapp/utils/DateUtil.java
+++ app/src/main/java/ru/geekbrains/weatherapp/utils/DateUtil.java
@@ -6,19 +6,19 @@ import java.util.Locale;
 
 public class DateUtil {
 
-    public static String getDayOfWeek(Locale locale){
+    public static String getDayOfWeek(Locale locale) {
         SimpleDateFormat sdf = new SimpleDateFormat("EEEE", locale);
         Date d = new Date();
         return sdf.format(d);
     }
 
-    public static String getMonth(Locale locale){
+    public static String getMonth(Locale locale) {
         SimpleDateFormat sdf = new SimpleDateFormat("MMMM", locale);
         Date d = new Date();
         return sdf.format(d);
     }
 
-    public static String getDayOfMonth(Locale locale){
+    public static String getDayOfMonth(Locale locale) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd", locale);
         Date d = new Date();
         return sdf.format(d);
