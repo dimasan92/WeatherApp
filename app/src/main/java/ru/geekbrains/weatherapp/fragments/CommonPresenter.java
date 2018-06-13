aaa758ed112ddd71cdc13aa2f2854e722c2208c8
dimasan92 <dimasan92@gmail.com>
2018-06-14 00:40:12 +0300

    refactor presenter

--- app/src/main/java/ru/geekbrains/weatherapp/fragments/CommonPresenter.java
+++ app/src/main/java/ru/geekbrains/weatherapp/fragments/CommonPresenter.java
@@ -1,15 +1,47 @@
 package ru.geekbrains.weatherapp.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import ru.geekbrains.weatherapp.common.Constants;
+import ru.geekbrains.weatherapp.common.Model;
 
 public class CommonPresenter extends Fragment {
 
-    public void assignModel(FragmentActivity activity) {
+    protected CommonView mView;
+    protected Model mModel;
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    public void attachView(CommonView view) {
+        mView = view;
     }
 
     public void detachView() {
+        mView = null;
+    }
 
+    public void viewIsReady() {
+
+    }
+
+    public void assignModel(FragmentActivity activity) {
+        FragmentManager fm = activity.getSupportFragmentManager();
+        mModel = (Model) fm.findFragmentByTag(Constants.MODEL_TAG);
+
+        if (mModel == null) {
+            mModel = Model.newInstance();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(mModel, Constants.MODEL_TAG);
+            ft.commit();
+        }
     }
 }
--- app/src/main/java/ru/geekbrains/weatherapp/fragments/welcome/WelcomePresenter.java
+++ app/src/main/java/ru/geekbrains/weatherapp/fragments/welcome/WelcomePresenter.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
+import java.util.Objects;
 import java.util.Set;
 
 import ru.geekbrains.weatherapp.R;
@@ -16,39 +17,21 @@ import ru.geekbrains.weatherapp.fragments.addcity.AddCityDialog;
 import ru.geekbrains.weatherapp.fragments.weather.WeatherFragment;
 
 public class WelcomePresenter extends CommonPresenter {
-    private WelcomeView mFragment;
-    private Model mModel;
 
     public static WelcomePresenter newInstance() {
         return new WelcomePresenter();
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRetainInstance(true);
-    }
-
-    public void attachView(WelcomeView fragment) {
-        mFragment = fragment;
-    }
-
-    public void detachView() {
-        mFragment = null;
-    }
-
     public void viewIsReady() {
         Set<String> setOfCities = mModel.getCities();
         String[] cities = setOfCities.toArray(new String[setOfCities.size()]);
-        mFragment.updateListView(cities);
+        ((WelcomeView) mView).updateListView(cities);
     }
 
     public void onItemClick(String city) {
-        if (getActivity() == null) {
-            return;
-        }
-
-        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        FragmentTransaction ft = Objects.requireNonNull(getActivity())
+                .getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.main_fragment, WeatherFragment
                 .newInstance(createBundle(city)));
         ft.addToBackStack(null);
@@ -65,22 +48,9 @@ public class WelcomePresenter extends CommonPresenter {
         dialog.show(fm, Constants.ADD_CITY_DIALOG_TAG);
     }
 
-    public void assignModel(FragmentActivity activity) {
-
-        FragmentManager fm = activity.getSupportFragmentManager();
-        mModel = (Model) fm.findFragmentByTag(Constants.MODEL_TAG);
-
-        if (mModel == null) {
-            mModel = Model.newInstance();
-            FragmentTransaction ft = fm.beginTransaction();
-            ft.add(mModel, Constants.MODEL_TAG);
-            ft.commit();
-        }
-    }
-
     private Bundle createBundle(String city) {
         Bundle bundle = new Bundle();
-        Bundle tmpBundle = mFragment.getArguments();
+        Bundle tmpBundle = mView.getArguments();
         if (tmpBundle != null) {
             bundle.putString(Constants.CITY_NAME, city);
             bundle.putBoolean(Constants.PARAM_PRESSURE, tmpBundle.getBoolean(Constants.PARAM_PRESSURE));
