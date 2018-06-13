46c5c7d3f858797b6d32ade8136a28bfd3119b4a
dimasan92 <dimasan92@gmail.com>
2018-06-13 23:51:24 +0300

    change StartActivity

--- app/src/main/java/ru/geekbrains/weatherapp/StartActivity.java
+++ app/src/main/java/ru/geekbrains/weatherapp/StartActivity.java
@@ -1,13 +1,14 @@
 package ru.geekbrains.weatherapp;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import ru.geekbrains.weatherapp.common.Constants;
 import ru.geekbrains.weatherapp.common.Model;
-import ru.geekbrains.weatherapp.fragments.choosecity.ChooseCityPresenter;
+import ru.geekbrains.weatherapp.fragments.welcome.WelcomeFragment;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -17,13 +18,21 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         FragmentManager fm = getSupportFragmentManager();
-        Model model = (Model) fm.findFragmentByTag(Constants.MODEL_TAG);
 
+        Model model = (Model) fm.findFragmentByTag(Constants.MODEL_TAG);
         if (model == null) {
             model = Model.newInstance();
             FragmentTransaction ft = fm.beginTransaction();
             ft.add(model, Constants.MODEL_TAG);
             ft.commit();
+        }
+
+        Fragment fragment = fm.findFragmentById(R.id.main_fragment);
+        if (fragment == null) {
+            fragment = WelcomeFragment.newInstance();
+            fm.beginTransaction()
+                    .add(R.id.main_fragment, fragment)
+                    .commit();
         }
     }
 }
