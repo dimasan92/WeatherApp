9f3a4d72900ff8187d9fc68482f6b008283d3f40
dimasan92 <dimasan92@gmail.com>
2018-06-17 16:59:45 +0300

    Rename DataModel on SimpleDataModel and implements interface DataModel

--- app/src/main/java/ru/geekbrains/weatherapp/model/DataModel.java
+++ app/src/main/java/ru/geekbrains/weatherapp/model/DataModel.java
@@ -1,75 +1,10 @@
 package ru.geekbrains.weatherapp.model;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
-import ru.geekbrains.weatherapp.common.Constants;
-import ru.geekbrains.weatherapp.common.listener.Observer;
-import ru.geekbrains.weatherapp.common.listener.Subject;
-import ru.geekbrains.weatherapp.fragments.welcome.WelcomePresenter;
+public interface DataModel {
 
-public class DataModel extends Fragment implements Subject{
+    boolean addCity(String cityName);
 
-    private SharedPreferences mSharedPreferences;
-    private Set<String> mData;
-
-    private List<Observer> mObservers;
-
-    public static DataModel newInstance() {
-        return new DataModel();
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRetainInstance(true);
-
-        mObservers = new ArrayList<>();
-
-        mSharedPreferences = Objects.requireNonNull(getActivity())
-                .getPreferences(Context.MODE_PRIVATE);
-        mData = mSharedPreferences.getStringSet(Constants.SAVED_SET_OF_CITIES, new HashSet<>());
-    }
-
-    @Override
-    public void registerObserver(Observer o) {
-        mObservers.add(o);
-    }
-
-    @Override
-    public void removeObserver(Observer o) {
-        mObservers.remove(o);
-    }
-
-    @Override
-    public void notifyObservers() {
-        for (Observer o : mObservers) {
-            o.update();
-        }
-    }
-
-    public boolean addCity(String cityName) {
-        mData.add(cityName);
-        mSharedPreferences = Objects.requireNonNull(getActivity())
-                .getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = mSharedPreferences.edit();
-        editor.remove(Constants.SAVED_SET_OF_CITIES).apply();
-        editor.putStringSet(Constants.SAVED_SET_OF_CITIES, mData);
-        editor.apply();
-        notifyObservers();
-        return true;
-    }
-
-    public Set<String> getData() {
-        return mData;
-    }
+    Set<String> getCities();
 }
---
+++ app/src/main/java/ru/geekbrains/weatherapp/model/SimpleDataModel.java
@@ -0,0 +1,76 @@
+package ru.geekbrains.weatherapp.model;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import ru.geekbrains.weatherapp.common.Constants;
+import ru.geekbrains.weatherapp.common.listener.Observer;
+import ru.geekbrains.weatherapp.common.listener.Subject;
+
+public class SimpleDataModel extends Fragment implements Subject, DataModel{
+
+    private SharedPreferences mSharedPreferences;
+    private Set<String> mCities;
+
+    private List<Observer> mObservers;
+
+    public static SimpleDataModel newInstance() {
+        return new SimpleDataModel();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+
+        mObservers = new ArrayList<>();
+
+        mSharedPreferences = Objects.requireNonNull(getActivity())
+                .getPreferences(Context.MODE_PRIVATE);
+        mCities = mSharedPreferences.getStringSet(Constants.SAVED_SET_OF_CITIES, new HashSet<>());
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        mObservers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        mObservers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : mObservers) {
+            o.update();
+        }
+    }
+
+    @Override
+    public boolean addCity(String cityName) {
+        mCities.add(cityName);
+        mSharedPreferences = Objects.requireNonNull(getActivity())
+                .getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.remove(Constants.SAVED_SET_OF_CITIES).apply();
+        editor.putStringSet(Constants.SAVED_SET_OF_CITIES, mCities);
+        editor.apply();
+        notifyObservers();
+        return true;
+    }
+
+    @Override
+    public Set<String> getCities() {
+        return mCities;
+    }
+}
