5efdff6c53c180d2a381ed4406baf908d62a3bdc
dimasan92 <dimasan92@gmail.com>
2018-06-25 05:26:45 +0300

    adding constants to interact with the service

--- app/src/main/java/ru/geekbrains/weatherapp/common/Constants.java
+++ app/src/main/java/ru/geekbrains/weatherapp/common/Constants.java
@@ -2,6 +2,10 @@ package ru.geekbrains.weatherapp.common;
 
 public class Constants {
 
+    // service constants
+    public static final String WEATHER_SERVICE_UPDATE = "weather_service_update";
+    public static final String WEATHER_SERVICE_FINISH = "weather_service_finish";
+
     // params
     public static final String CITY_NAME = "city_name";
     public static final String PARAM_PRESSURE = "param_pressure";
