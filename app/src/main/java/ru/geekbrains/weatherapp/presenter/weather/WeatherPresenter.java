a8252b73fb91e49bd3d8ca0f9e672870e47c00bd
dimasan92 <dimasan92@gmail.com>
2018-06-25 06:08:11 +0300

    get data from service

--- app/src/main/java/ru/geekbrains/weatherapp/presenter/weather/WeatherPresenter.java
+++ app/src/main/java/ru/geekbrains/weatherapp/presenter/weather/WeatherPresenter.java
@@ -1,12 +1,16 @@
 package ru.geekbrains.weatherapp.presenter.weather;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import java.util.Locale;
 
 import ru.geekbrains.weatherapp.common.Constants;
+import ru.geekbrains.weatherapp.model.weathermodel.WeatherService;
 import ru.geekbrains.weatherapp.presenter.Presenter;
 import ru.geekbrains.weatherapp.view.IView;
 import ru.geekbrains.weatherapp.view.screens.weather.IWeatherView;
@@ -14,6 +18,8 @@ import ru.geekbrains.weatherapp.view.screens.weather.IWeatherView;
 public class WeatherPresenter extends Presenter implements IWeatherPresenter {
 
     private IWeatherView mView;
+    private FinishReceiver mFinishReceiver;
+    private UpdateReceiver mUpdateReceiver;
 
     public static WeatherPresenter newInstance(Bundle bundle) {
         WeatherPresenter presenter = new WeatherPresenter();
@@ -41,6 +47,13 @@ public class WeatherPresenter extends Presenter implements IWeatherPresenter {
     public void viewIsReady(Context context) {
         setSettings();
         setDate(context);
+
+        mView.setWind("Загрузка данных");
+
+        Intent intent = new Intent(context, WeatherService.class);
+        context.startService(intent);
+
+        registerReceivers(context);
     }
 
     private void setSettings() {
@@ -63,5 +76,41 @@ public class WeatherPresenter extends Presenter implements IWeatherPresenter {
                 mModel.time().getMonth(locale)));
 
         mView.setDayOfWeek(mModel.time().getDayOfWeek(locale));
+    }
+
+    private void registerReceivers(Context context){
+        mFinishReceiver = new FinishReceiver();
+        mUpdateReceiver = new UpdateReceiver();
+
+        IntentFilter intentFilter = new IntentFilter(
+                Constants.ACTION_WEATHER_SERVICE_FINISH);
+        intentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        context.registerReceiver(mFinishReceiver, intentFilter);
+
+        // Регистрируем второй приёмник
+        IntentFilter updateIntentFilter = new IntentFilter(
+                Constants.ACTION_WEATHER_SERVICE_UPDATE);
+        updateIntentFilter.addCategory(Intent.CATEGORY_DEFAULT);
+        context.registerReceiver(mUpdateReceiver, updateIntentFilter);
+    }
+
+    public class FinishReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String result = intent
+                    .getStringExtra(Constants.EXTRA_WEATHER_SERVICE_FINISH);
+            mView.setWind(result);
+        }
+    }
+
+    public class UpdateReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String result = intent
+                    .getStringExtra(Constants.EXTRA_WEATHER_SERVICE_UPDATE);
+            mView.setWind(result);
+        }
     }
 }
