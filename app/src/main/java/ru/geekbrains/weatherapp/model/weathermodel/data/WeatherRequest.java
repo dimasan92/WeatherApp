6fdf627d5597db7377c868ad1c0bcf923fcd1b32
dimasan92 <dimasan92@gmail.com>
2018-07-05 13:13:30 +0300

    add description for weather

--- app/src/main/java/ru/geekbrains/weatherapp/model/weathermodel/data/WeatherRequest.java
+++ app/src/main/java/ru/geekbrains/weatherapp/model/weathermodel/data/WeatherRequest.java
@@ -3,6 +3,8 @@ package ru.geekbrains.weatherapp.model.weathermodel.data;
 import com.google.gson.annotations.SerializedName;
 
 public final class WeatherRequest {
+    @SerializedName("weather")
+    private Weather[] weather;
     @SerializedName("main")
     private final Main main;
     @SerializedName("wind")
@@ -14,12 +16,17 @@ public final class WeatherRequest {
     @SerializedName("cod")
     private final int cod;
 
-    public WeatherRequest(Main main, Wind wind, Clouds clouds, String name, int cod) {
+    public WeatherRequest(Weather[] weather, Main main, Wind wind, Clouds clouds, String name, int cod) {
+        this.weather = weather;
         this.main = main;
         this.wind = wind;
         this.clouds = clouds;
         this.name = name;
         this.cod = cod;
+    }
+
+    public Weather[] getWeather() {
+        return weather;
     }
 
     public Main getMain() {
