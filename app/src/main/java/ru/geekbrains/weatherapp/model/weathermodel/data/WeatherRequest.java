07af3c26ec0a01c8a71b9b6eb8e882398013b8aa
dimasan92 <dimasan92@gmail.com>
2018-07-05 05:46:49 +0300

    some changes in class with data from server

--- app/src/main/java/ru/geekbrains/weatherapp/model/weathermodel/data/WeatherRequest.java
+++ app/src/main/java/ru/geekbrains/weatherapp/model/weathermodel/data/WeatherRequest.java
@@ -2,55 +2,43 @@ package ru.geekbrains.weatherapp.model.weathermodel.data;
 
 import com.google.gson.annotations.SerializedName;
 
-public class WeatherRequest {
+public final class WeatherRequest {
     @SerializedName("main")
-    private Main main;
+    private final Main main;
     @SerializedName("wind")
-    private Wind wind;
+    private final Wind wind;
     @SerializedName("clouds")
-    private Clouds clouds;
+    private final Clouds clouds;
     @SerializedName("name")
-    private String name;
+    private final String name;
     @SerializedName("cod")
-    private int cod;
+    private final int cod;
+
+    public WeatherRequest(Main main, Wind wind, Clouds clouds, String name, int cod) {
+        this.main = main;
+        this.wind = wind;
+        this.clouds = clouds;
+        this.name = name;
+        this.cod = cod;
+    }
 
     public Main getMain() {
         return main;
-    }
-
-    public void setMain(Main main) {
-        this.main = main;
     }
 
     public Wind getWind() {
         return wind;
     }
 
-    public void setWind(Wind wind) {
-        this.wind = wind;
-    }
-
     public Clouds getClouds() {
         return clouds;
-    }
-
-    public void setClouds(Clouds clouds) {
-        this.clouds = clouds;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getCod() {
         return cod;
-    }
-
-    public void setCod(int cod) {
-        this.cod = cod;
     }
 }
